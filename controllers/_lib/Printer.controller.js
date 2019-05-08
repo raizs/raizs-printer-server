0a5e0761b8212801ea39fc79f2860b52db0f5bd6
Gabriel Morales <gabriel.morales@thevelops.com>
2019-05-08 16:18:33 -0300

    deleted printer as dependency and turned into server

--- controllers/_lib/Printer.controller.js
+++ controllers/_lib/Printer.controller.js
@@ -1,36 +1,34 @@
-import printer from "printer";
+// import printer from "printer";
 import fs from "fs";
-// import { spawn, exec } from 'child_process';
-// import { join } from 'async-child-process';
 import ngrok from "ngrok"
 import axios from "axios"
 
 export class PrinterController {
 
-	async print({body, files}) {
-	  	console.log('\n\nUHUL')
-	  	console.log(files)
-  	// const printers = printer.getPrinters()
-  	// console.log(printers)
-	 //  	printer.printDirect({data:files.file.data,
-	 //  		type:"pdf",
-		//     printer: "Raizs-Mada", // printer name, if missing then will print to default printer
-		//     success:function(jobID){
-		//       console.log("sent to printer with ID: "+jobID);
-		//     },
-		//     error:function(err){
-		//       console.log(err);
-		//     }
-		// });
+	// async print({body, files}) {
+	//   	console.log('\n\nUHUL')
+	//   	console.log(files)
+ //  	// const printers = printer.getPrinters()
+ //  	// console.log(printers)
+	//  //  	printer.printDirect({data:files.file.data,
+	//  //  		type:"pdf",
+	// 	//     printer: "Raizs-Mada", // printer name, if missing then will print to default printer
+	// 	//     success:function(jobID){
+	// 	//       console.log("sent to printer with ID: "+jobID);
+	// 	//     },
+	// 	//     error:function(err){
+	// 	//       console.log(err);
+	// 	//     }
+	// 	// });
 
- //  	fs.readFile(__dirname + '/file.pdf', function (err, data) {
- //  		console.log(data)
-	//   if (err) throw err;
-	// });
-		return {
-			"success": true		
-		}
-   }
+ // //  	fs.readFile(__dirname + '/file.pdf', function (err, data) {
+ // //  		console.log(data)
+	// //   if (err) throw err;
+	// // });
+	// 	return {
+	// 		"success": true		
+	// 	}
+ //   }
 
 
 	async startServer(){
